6ef589d44276d9ca4bc30c3fe4580d1f9356f868
Stefan Wysocki <stefan.wysocki@tietoevry.com>
2020-04-06 15:04:41 +0200

    Add simple database

--- vssfeeder.js
+++ vssfeeder.js
@@ -4,7 +4,6 @@ var client = new net.Socket();
 
 var parseString = require('xml2js').parseString;
 
-
 client.connect(5678, '127.0.0.1', function() {
 	console.log('Connected! Subscribing.');
 	client.write('<?xml version="1.0" encoding="UTF-8"?>\n');
@@ -12,10 +11,57 @@ client.connect(5678, '127.0.0.1', function() {
 	client.write('</Message>\n');
 });
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+// TODO Database schema should be generated from vspec
+db.defaults({
+	"Vehicle": {
+	  "VersionVSS": {
+		"Major": 2,
+		"Minor": 0,
+		"Patch": 0
+	  },
+	  "Drivetrain": {
+		"InternalCombustionEngine": {
+		  "Engine": {
+			"Speed": {}
+		  }
+		},
+		"Transmission": {
+		  "Gear": {}
+		},
+		"FuelSystem": {
+		  "TankCapacity": {},
+		  "Level": {},
+		  "InstantConsumption": {}
+		}
+	  }
+	}
+  })
+  .write()
+
 client.on('data', function(data) {
 	parseString(data, function (err, result) {
 		if (result.Message.Event[0].$.Name == "SubscribedValues") {
-			console.log(result.Message.Event[0].root[0].thisVehicle[0].exterior[0].engineCompartment[0].engine[0].Properties[0].actualRpm[0]);
+			// TODO Make those 2 values system mapped more generically
+			var exterior = result.Message.Event[0].root[0].thisVehicle[0].exterior[0];
+			var rpmValue = exterior.engineCompartment[0].engine[0].Properties[0].actualRpm[0];
+			var fuelproperties = exterior.fueling[0].fuelType[0].tank[0].Properties[0];
+			var maxAmount = fuelproperties.maxAmount;
+			var actualAmount = fuelproperties.actualAmount;
+			var currentConsumption = fuelproperties.currentConsumption;
+			var currentGear = exterior.gearUnit[0].Properties[0].currentGear[0];
+		
+			db.set('Vehicle.Drivetrain.InternalCombustionEngine.Engine.Speed', rpmValue)
+			.set('Vehicle.Drivetrain.FuelSystem.TankCapacity', maxAmount)
+			.set('Vehicle.Drivetrain.FuelSystem.Level',actualAmount/maxAmount*100)
+			.set('Vehicle.Drivetrain.FuelSystem.InstantConsumption', currentConsumption)
+			.set("Vehicle.Drivetrain.Transmission.Gear", currentGear)
+			.write();
 		}
 	});
 });
