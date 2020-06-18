4e284b752e78d8a176a4c534e65b1c39367cbee5
Piotr Krawczyk <piotr.krawczyk@tietoevry.com>
2020-06-18 18:10:41 +0200

    Use sqlite3 for sharing db.

--- vssfeeder.js
+++ vssfeeder.js
@@ -5,73 +5,81 @@
 
 var net = require('net');
 
+var xpath = require('xpath')
+	, dom = require('xmldom').DOMParser;
+
 var client = new net.Socket();
 
-var parseString = require('xml2js').parseString;
-
-client.connect(5678, '127.0.0.1', function() {
+client.connect(5678, '10.0.2.2', function() {
 	console.log('Connected! Subscribing.');
 	client.write('<?xml version="1.0" encoding="UTF-8"?>\n');
 	client.write('<Message><Event Name="EstablishConnection"/>\n');
 	client.write('</Message>\n');
 });
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-
-const adapter = new FileSync('/usr/shared/db/db.json')
-const db = low(adapter)
-
-// TODO Database schema should be generated from vspec
-db.defaults({
-	"vehicle": {
-	  "versionVSS": {
-		"major": 2,
-		"minor": 0,
-		"patch": 0
-	  },
-	  "drivetrain": {
-		"internalCombustionEngine": {
-		  "engine": {
-			"speed": {}
-		  }
-		},
-		"transmission": {
-		  "gear": {}
-		},
-		"fuelSystem": {
-		  "tankCapacity": {},
-		  "level": {},
-		  "instantConsumption": {}
-		}
-	  }
+const sqlite3 = require("sqlite3")
+const dbPath = "/usr/shared/db/opends.db";
+let sqlite_db = new sqlite3.Database(dbPath, (err) => {
+	if (err) {
+		return console.error(err.message);
 	}
-  })
-  .write()
+	console.log('Connected to the opends database.');
+});
 
+let values_mapping = new Map();
+values_mapping.set('drivetrain.internalCombustionEngine.engine.speed', '/Message/Event/root/thisVehicle/exterior/engineCompartment/engine/Properties/actualRpm');
+values_mapping.set('speed', '/Message/Event/root/thisVehicle/physicalAttributes/Properties/speed');
+values_mapping.set('drivetrain.transmission.gear', '/Message/Event/root/thisVehicle/exterior/gearUnit/Properties/currentGear');
+values_mapping.set('drivetrain.fuelsystem.tankCapacity', '/Message/Event/root/thisVehicle/exterior/fueling/fuelType/tank/Properties/maxAmount');
+values_mapping.set('drivetrain.fuelSystem.level', '/Message/Event/root/thisVehicle/exterior/fueling/fuelType/tank/Properties/actualAmount');
+values_mapping.set('drivetrain.fuelsystem.instantConsumption', '/Message/Event/root/thisVehicle/exterior/fueling/fuelType/tank/Properties/currentConsumption');
+createSchema(sqlite_db);
+
+client.setEncoding('utf-8');
 client.on('data', function(data) {
-	parseString(data, function (err, result) {
-		if (result.Message.Event[0].$.Name == "SubscribedValues") {
-			// TODO Make those 2 values system mapped more generically
-			var exterior = result.Message.Event[0].root[0].thisVehicle[0].exterior[0];
-			var rpmValue = exterior.engineCompartment[0].engine[0].Properties[0].actualRpm[0];
-			var fuelproperties = exterior.fueling[0].fuelType[0].tank[0].Properties[0];
-			var maxAmount = fuelproperties.maxAmount[0];
-			var actualAmount = fuelproperties.actualAmount;
-			var currentConsumption = fuelproperties.currentConsumption;
-			var currentGear = exterior.gearUnit[0].Properties[0].currentGear[0];
-		
-			db.set('vehicle.drivetrain.internalCombustionEngine.engine.speed', rpmValue)
-			.set('vehicle.drivetrain.fuelSystem.tankCapacity', maxAmount)
-			.set('vehicle.drivetrain.fuelSystem.level',Math.floor(actualAmount / maxAmount * 100))
-			.set('vehicle.drivetrain.fuelSystem.instantConsumption', currentConsumption)
-			.set("vehicle.drivetrain.transmission.gear", currentGear)
-			.write();
-		}
-	});
+	var doc = new dom().parseFromString(data);
+	var query = sqlite_db.prepare("REPLACE INTO vss_data (key, value, timestamp) VALUES(?, ?," + Date.now() + ");");
+	for (let [key, value] of values_mapping) {
+		query.run(key, xpath.select("string(" + value + ")", doc));
+		console.log(key + ": " + xpath.select("string(" + value + ")", doc));
+	}
 });
 
 client.on('close', function() {
+	sqlite_db.close((err) => {
+		if (err) {
+			console.error(err.message);
+		}
+		console.log('Opends database connection closed.');
+	});
 	console.log('Connection closed');
 });
 
+function createSchema(db) {
+	var query = `CREATE TABLE IF NOT EXISTS vss_data (
+					key TEXT PRIMARY KEY NOT NULL UNIQUE,
+					value TEXT,
+					timestamp TEXT NOT NULL
+				   );`;
+	db.exec(query, (err) => {
+		if (err) {
+			return console.error(err.message);
+		}
+		console.log('Schema created.');
+	});
+	query = `CREATE TABLE IF NOT EXISTS vss_schema_version (
+			  major INTEGER NOT NULL,
+			  minor INTEGER NOT NULL,
+			  patch INTEGER NOT NULL
+			);`;
+	db.exec(query, (err) => {
+		if (err) {
+			return console.error(err.message);
+		}
+		console.log('Schema created.');
+	});
+	query = "INSERT OR REPLACE INTO vss_data (key, value, timestamp) VALUES(?, ?," + Date.now() + ");";
+	for (let [key, value] of values_mapping) {
+		db.run(query, [key, ""])
+	}
+}
